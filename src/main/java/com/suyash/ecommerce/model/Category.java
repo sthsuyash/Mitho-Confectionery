71781546bf630334e87791c71e249bcd60d738c0
Suyash Shrestha <sthasuyash11@gmail.com>
2022-09-13 16:27:32 +0545

    updated category model

--- src/main/java/com/suyash/ecommerce/model/Category.java
+++ src/main/java/com/suyash/ecommerce/model/Category.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Data
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -14,4 +13,31 @@ public class Category {
     @Column(name = "category_name")
     private String name;
 
+    public Category() {
+    }
+
+    public Category(Long id) {
+        this.id = id;
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
